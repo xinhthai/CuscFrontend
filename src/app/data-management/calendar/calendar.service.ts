a985ecaabcaee9185b365fcb490671c659f0ccd2
myxinh <ttmxinh1997@gmail.com>
2021-01-03 14:27:45 +0700

    update calendar service

--- src/app/data-management/calendar/calendar.service.ts
+++ src/app/data-management/calendar/calendar.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SERVER_API_URL } from 'src/app/app.constants';
+import { Calendar } from './calendar.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  addCalendar(calendar : Calendar): Observable<any>{
+    return this.http.post(SERVER_API_URL + '/lits', calendar);
+  };
 }
--- src/app/data-management/calendar/create-calendar/create-calendar.component.ts
+++ src/app/data-management/calendar/create-calendar/create-calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Calendar } from '../calendar.model';
+import { CalendarService } from '../calendar.service';
 @Component({
   selector: 'app-create-calendar',
   templateUrl: './create-calendar.component.html',
@@ -8,9 +9,17 @@ import { Calendar } from '../calendar.model';
 export class CreateCalendarComponent implements OnInit {
   Calendar: Calendar=new Calendar();
   checked=true;
-  constructor() { }
+  constructor(private calendarService : CalendarService) { }
 
   ngOnInit(): void {
   }
-
+  addCalendar(){
+    console.log(this.Calendar);
+    this.calendarService.addCalendar(this.Calendar).subscribe(
+      data =>{
+        console.log('Thêm lịch tuyển sinh thành công');
+      },
+      error =>console.log(error)
+    );
+  }
 }
